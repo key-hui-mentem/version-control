bc4661bf624a2da301d8a999d09434d42d33c3bb
Key Hui <key.hui@mentem.co>
2023-01-11 21:24:16 +0000

    fix: remove real slack url

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 version_label=v1.0.0
-slack_webhook=https://hooks.slack.com/services/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
+slack_webhook=https://hooks.slack.com/services/xxxxxxx/xxxxxxx
 sh ./.github/workflows/slack.sh version_control $version_label $slack_webhook
