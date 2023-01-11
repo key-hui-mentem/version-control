ff78b9517e5539a99314806eba43e58ba1c55be1
Key Hui <key.hui@mentem.co>
2023-01-11 21:09:34 +0000

    fix: slack webhook

--- .github/workflows/slack.sh
+++ .github/workflows/slack.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
